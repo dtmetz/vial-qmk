d1dbdc66cd3ad436c846411bf344740762209708
Daniel Metz <danielmetz@Daniels-MacBook-Pro.local>
2024-08-26 22:48:00 -0500

    Support for Sofle Vial

---
+++ keyboards/sofle2/keymaps/keyhive_vial/rules.mk
@@ -0,0 +1,21 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+VIAL_INSECURE = yes
+
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = yes
+
+# RGB_MATRIX_ENABLE = WS28127
